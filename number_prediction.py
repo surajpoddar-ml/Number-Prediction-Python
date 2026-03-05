9fbc36c447c2bac592dcf631bedc8929c0376a23
suraj <surajpoddar098@gmail.com>
2026-03-06 00:15:51 +0545

    Adding main and error handling

--- number_prediction.py
+++ number_prediction.py
@@ -1,18 +1,22 @@
 import random
 
-number =random.randint(1,100)
-guess=0
+def main():
+    number = random.randint(1, 100)
+    guess = 0
 
-while guess!=number:
-    print("Please enter the number between 1-100 !")
+    while guess != number:
+        try:
+            guess = int(input("Enter a number between 1-100 :"))
 
-    guess=int(input("Enter a number between 1-100 :"))
+            if(guess > number):
+                print("Too high")
+            elif(guess < number):
+                print("Too low")
+            else:
+                print("Congratulations! You have guessed it true.")
+                break
+        except ValueError:
+            print("Please enter a number between 1-100")
 
-    if(guess > number):
-        print("Too high")
-    elif(guess<number):
-        print("Too low")
-    else:
-        print("Congratulations! You have guessed it true.")
-              
-    
+
+main()
